771e6c7a45b4ca28e50f7620db290803a47770bc
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-07-14 16:17:13 -0400

    self-host vanilla-tilt

--- assets/home.js
+++ assets/home.js
@@ -1,16 +1,3 @@
-function isMobileDevice() {
-    return window.matchMedia("(pointer: coarse)").matches;
-}
-if (!isMobileDevice()) {
-    const script = document.createElement("script");
-    script.src =
-        "https://cdn.jsdelivr.net/npm/vanilla-tilt@1.8.1/dist/vanilla-tilt.min.js";
-    script.onload = function () {
-        VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
-    };
-    document.head.appendChild(script);
-}
-
 function runScrambleAnimation(element) {
     const originalText = element.textContent;
     if (!originalText) return;
